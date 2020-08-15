afac79a3afb40a419c1d5c5d56106718617df904
Adam B <freddyyeddy@gmail.com>
2020-08-15 11:36:48 -0800

    Did Some Tweaks To the Hero Image Block
    I swapped over to doing psudo overlay. so i can properly filter. Still minor tweaks left to do but can safely move on to the next block for now. I also mimicked the color swatches from example. Also update of ACF plugin

--- wp-content/themes/JointsWP/parts/blocks/hero/hero.php
+++ wp-content/themes/JointsWP/parts/blocks/hero/hero.php
@@ -15,9 +15,16 @@ if( !empty($block['align']) ) {
 }
 
 ?>
-<div style="background-image: linear-gradient(to right,      rgba(0, 0, 0, 0),      rgb(0 0 0 / 0.25)), url(<?php echo get_field('background') ?>)" id="<?php echo esc_attr($id); ?>" class="hero-section Overlay-Container <?php echo esc_attr($className); ?>">
+
+<style>
+    .hero-section::before {
+        background-image: url(<?php echo get_field('background') ?>);
+    }
+</style>
+<div class="hero-section Overlay-Container <?php echo esc_attr($className); ?>">
   <div class="hero-section-text">
     <h1><?php echo get_field("herotitle"); ?></h1>
     <h5><?php echo get_field('herotext'); ?></h5>
+    <a style="text-shadow: none;" class="button" href="https://demo.easyappointments.org/">Schedule a Visit</a>
   </div>
 </div>
